611a805ff23d8137ed57a304e0122adfbbf22699
124328227@qq.com <124328227@qq.com>
2018-11-16 18:42:07 +0800

    V1.0-R-1 test

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastClick'
+/* 重置页面样式表 统一不同手机的页面样式 */
+import './assets/styles/reset.css'
+/* 解决1像素边框问题 */
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+/* 解决移动端300毫秒点击事件延迟
+ * cnpm install fastclick --save */
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
