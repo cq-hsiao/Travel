1b15a70991cdb125e66fcdda080f751b16a421c1
124328227@qq.com <124328227@qq.com>
2018-11-16 17:21:12 +0800

    V1.0-R-1 test

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+        <div>Home</div>
+        <router-link to="/list">列表页面</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
