74c16aee2dd3c2f15cb21283ba17febfc731c56f
124328227@qq.com <124328227@qq.com>
2018-11-23 18:12:33 +0800

    Vdetail-list 加入基础动画

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -7,15 +7,19 @@
 				<div class="banner-number"><span class="iconfont banner-icon">&#xe95b;</span>39</div>
 			</div>
 		</div>
-		<common-gallary
-						:imgs="bannerImgs"
-						v-show="showGallary"
-						@close="handleGallaryClose"></common-gallary>
+		<fade-animation>
+			<common-gallary
+							:imgs="bannerImgs"
+							v-show="showGallary"
+							@close="handleGallaryClose">
+			</common-gallary>
+		</fade-animation>
 	</div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
 	name: 'DetailDetail',
 	props: {
@@ -37,7 +41,8 @@ export default {
 		}
 	},
 	components: {
-		CommonGallary
+		CommonGallary,
+		FadeAnimation
 	}
 }
 </script>
